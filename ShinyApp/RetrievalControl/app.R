b27366080782767cd26057a930adf2198763fc2e
Mrinmayi <mrinmayi.17@gmail.com>
2024-02-05 13:01:14 -0500

    Added all illustrations

--- ShinyApp/RetrievalControl/app.R
+++ ShinyApp/RetrievalControl/app.R
@@ -16,7 +16,7 @@ library(ggplot2)
 
 #First setup some paths etc.
 if(grepl("*[M|m]rinmayi*", Sys.info()['nodename'])){
-  setwd("~/GitDir/ConcealMemory/ShinyApp/RetrievalControl/")
+  setwd("~/GitDir/MemoryControlEyeMovements/ShinyApp/RetrievalControl/")
 }
 
 # Define UI for application that draws a histogram
@@ -25,14 +25,14 @@ ui <- fluidPage(
   includeCSS('www/customstylesheet.css'),
   includeCSS('https://fonts.googleapis.com/icon?family=Material+Icons'),
   title = "Memory Control",
-  
   dashboardPage(
-    dashboardHeader(title="Memory Control"),
+    dashboardHeader(title=""),
     dashboardSidebar(
       sidebarMenu(
-        menuItem("Introduction", tabName= "introduction", icon=tags$i(class = "material-icons", "people", style = "font-size:inherit;")),
-        menuItem("Method", tabName= "method", icon=tags$i(class = "material-icons", "construction", style = "font-size:inherit;")),
-        menuItem("Results", tabName= "results", icon=tags$i(class = "material-icons", "bar_chart", style = "font-size:inherit;"))
+        menuItem("Background", tabName= "background", icon=tags$i(class = "material-icons", "article", style = "font-size:inherit;")),
+        menuItem("The Study", tabName= "study", icon=tags$i(class = "material-icons", "bar_chart", style = "font-size:inherit;")),
+        menuItem("Conclusion", tabName= "conclusion", icon=tags$i(class = "material-icons", "bar_chart", style = "font-size:inherit;")),
+        menuItem("Related Material", tabName= "materials", icon=tags$i(class = "material-icons", "link", style = "font-size:inherit;"))
       ) #Closes sidebarMenu
     ), #Closes dashboardSidebar
     dashboardBody(
@@ -42,8 +42,8 @@ ui <- fluidPage(
                            type = "text/css", href = "https://fonts.googleapis.com/icon?family=Material+Icons")),
       
       tabItems(
-        ########## Method ##########
-        tabItem(tabName = "introduction",
+        ########## Background ##########
+        tabItem(tabName = "background",
                 fluidRow(
                   box(width = 12,
                       title = "What is the goal of the study?"
@@ -52,6 +52,8 @@ ui <- fluidPage(
                       title = "Why should we care?",
                       p("Although the ability to remember past events is essential for daily living, it is not always beneficial."),
                       p("Imagine someone who has experienced a traumatic event such as getting mugged in a park. In the future, a walk the park may remind them of the traumatic experience of being mugged - which is not pleasant."),
+                      p("WHY SUPPRESS?"),
+                      img(src = "PatternCompletion.gif", width = "50%", height = "50%", offset = 0, style='padding:0px;'),
                       p("In fact, such 'flashes' of memory is a common feature of conditions such as Post Traumatic Stress Disorder (PTSD) or Major Depressive Disorder.")
                   ), #Close importance box
                   box(width = 12,
@@ -59,21 +61,54 @@ ui <- fluidPage(
                       p("Studies have suggested that we can use two strategies to control memory retrieval."),
                       h4("Retrieval Suppression"),
                       p("This involves pushing the retrieved information out of your awareness."),
+                      img(src = "Suppress.gif", width = "50%", height = "50%", offset = 0, style='padding:0px;'),
                       h4("Thought Substitution"),
-                      p("This involves calling to mind alternate information instead of the retrieved memory.")
+                      p("This involves calling to mind alternate information instead of the retrieved memory."),
+                      img(src = "Substitute.gif", width = "50%", height = "50%", offset = 0, style='padding:0px;')
                   ) #Close strategies box
                 ) #Close fluidRow
-        ), #Close tabName
-        tabItem(tabName = "method",
+        ), #Close background tabItem
+        ########## Study ##########
+        tabItem(tabName = "study",
                 fluidRow(
                   box(width = 12,
-                      title = "Procedure of the experiment"
+                      title = "The Experiment",
+                      p("ENCODING TEXT"),
+                      img(src = "Encoding.gif", width = "25%", height = "25%", offset = 0, style='padding:0px;'),
+                      p("SEARCH TEXT"),
+                      img(src = "Search.gif", width = "25%", height = "25%", offset = 0, style='padding:0px;'),
+                      p("POST TEST TEXT"),
+                      img(src = "Posttest.gif", width = "25%", height = "25%", offset = 0, style='padding:0px;')
                   ), #Close procedure box
                   box(width = 12,
-                      title = "What do we expect?"
-                      )
+                      title = "The Results",
+                      p("Post-test Memory"),
+                      img(src = "PosttestAccuracy.png", width = "50%", height = "50%", offset = 0, style='padding:0px;'),
+                      p("Associate Viewing"),
+                      img(src = "AssociateViewing.png", width = "50%", height = "50%", offset = 0, style='padding:0px;')
+                  ) #Close results box
                 ) #Close fluidRow
-        )
+        ), #Close study tabItem
+        ########## Conclusion ##########
+        tabItem(tabName = "conclusion",
+                fluidRow(
+                  box(width = 12,
+                      title = "What do the results mean?"
+                  ) #Close discussion box
+                ) #Close fluidRow
+        ), #Close conclusion tabItem
+        ########## Related Materials ##########
+        tabItem(tabName = "materials",
+                fluidRow(
+                  box(width = 12,
+                      title = "Links to Related Reading",
+                      tags$ul(
+                        tags$li(tags$a(href="https://psycnet.apa.org/record/2024-38477-001", "Link to the published article")),
+                        tags$li(tags$a(href="https://osf.io/preprints/psyarxiv/q4fvs", "Link to open-access full text")),
+                        tags$li(tags$a(href="https://osf.io/preprints/psyarxiv/q4fvs", "Link to open-access full text")))
+                  ) #Close discussion box
+                ) #Close fluidRow
+        ) #Close conclusion tabItem
       ) #Close tabItems
     ) #Close dashboardBody
   ) #Close dashboardPage
